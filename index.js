7000a1dc9d4a7b4baf838cde466ad31f701d377c
Osamah Ahmad <me@osamahahmad.com>
2023-01-06 03:50:14 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static('build'))
+
+const http = require('http');
+const server = http.createServer(app);
+
+/*const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    socket.on('message', (e) => {
+        socket.broadcast.emit('message', e)
+    })
+})
+
+io.on('disconnect', (e) => {
+    console.log('Disconnect')
+})*/
+
+server.listen(3000);
